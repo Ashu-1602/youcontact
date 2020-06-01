1038b3a8c877d2ee4d1e3f7146de48ee74a65b46
ashutosh.s <a.sakalle@gmail.com>
2020-06-01 16:39:36 +0530

    Initial commit for youcontact WebApp

---
+++ youcontact/src/main/java/com/youconnect/YoucontactApplication.java
@@ -0,0 +1,13 @@
+package com.youconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoucontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoucontactApplication.class, args);
+	}
+
+}
---
+++ youcontact/src/test/java/com/youconnect/YoucontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoucontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
